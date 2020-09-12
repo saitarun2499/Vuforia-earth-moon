4f8879a7674c9fccf53e25970ae3208dca85851e
Singaraju Sriram Teja <ssriramteja@gmail.com>
2020-09-12 08:15:04 +0530

    First commit

---
+++ Assets/scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Rotate(Vector3.down * (5 * speed));
+    }
+}
